0e7af5c76a13cfadf980b0e3277c909c815f22b4
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-30 13:45:18 +0200

    Add authorization for findAll users

--- server/db.js
+++ server/db.js
@@ -2,5 +2,5 @@ import mongoose from 'mongoose';
 
 export default (callback) => {
     mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-starter');
-	callback();
+    callback();
 }
