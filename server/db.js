1277633f9a363e6109e56ed28b46a2e152b02097
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-30 13:46:07 +0200

    Mongodb url

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-starter');
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-starter-es6');
     callback();
 }
