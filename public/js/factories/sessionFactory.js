5eda8b260ebefb154be9676bcefe5bf54c9a3bc5
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-16 16:46:01 +0100

    Update sessionFactory.js

--- public/js/factories/sessionFactory.js
+++ public/js/factories/sessionFactory.js
@@ -1,8 +1,6 @@
-class sessionFactory {
-
-    constructor() {
-        this.user = {};
-        this.token = null;
+function sessionFactory() {
+    return {
+        user: {},
+        token: null
     }
-
 }
